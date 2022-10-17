c2dbe51027c22619616531b41e095fad97837877
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2022-10-17 02:36:22 -0300

    Entendendo Assumptions

---
+++ src/test/java/one/digitalinnovation/junit/AssumptionsTeste.java
@@ -0,0 +1,15 @@
+package one.digitalinnovation.junit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
+
+public class AssumptionsTeste {
+
+
+    @Test
+    void validarAlgoSomenteNoUsuarioPedro(){
+        Assumptions.assumeTrue("Pedro".equals(System.getenv("USER")));
+        Assertions.assertEquals(10, 5+5);
+    }
+}
---
+++ src/test/java/one/digitalinnovation/junit/CondicionaisTeste.kt
@@ -0,0 +1,4 @@
+package one.digitalinnovation.junit
+
+class CondicionaisTeste {
+}
